cdd1fb4122af6083ddeaafa2e2d1a2e9a7d28ca9
willianaurelio <willianaurelio@live.com>
2023-05-09 11:26:15 -0300

    tratamento de erro

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,18 +2,25 @@ import { UserProps } from "../types/user";
 
 import Search from "../components/Search";
 import User from "../components/User";
+import Error from "../components/Error";
 
 import { useState } from 'react';
 
 
 const Home = () => {
     const [user, setUser] = useState<UserProps | null>(null);
+    const [error, setError] = useState(false);
 
     const loadUser = async(userName: string) => {
 
         const res = await fetch(`https://api.github.com/users/${userName}`);
 
         const data = await res.json();
+
+        if(res.status === 404){
+            setError(true);
+            return;
+        }
         
         const { avatar_url, login, location, followers, following } = data;
 
@@ -32,6 +39,7 @@ const Home = () => {
         <div>
             <Search loadUser={loadUser} />
             {user && <User {...user} />}
+            {error && <Error />}
         </div>
     </div>
 }
