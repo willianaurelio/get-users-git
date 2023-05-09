674e537b3cfd6a2649c537dff691d87a9c0b270f
willianaurelio <willianaurelio@live.com>
2023-05-09 10:00:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Outlet} from 'react-router-dom';
+
+function App() {
+
+  return (
+    <>
+    <h1>GitHub Finder</h1>
+    <Outlet />
+    </>
+  )
+}
+
+export default App
---
+++ src/routes/Home.tsx
@@ -0,0 +1,28 @@
+import { UserProps } from "../types/user";
+
+import Search from "../components/Search";
+
+import { useState } from 'react';
+
+
+const Home = () => {
+    const [user, setUser] = useState<UserProps | null>(null);
+
+    const loadUser = async(userName: string) => {
+
+        const res = await fetch(`https://api.github.com/users/${userName}`);
+
+        const data = await res.json();
+        
+        console.log(data);
+    }
+
+    return <div>
+        <div>
+            <Search loadUser={loadUser} />
+
+        </div>
+    </div>
+}
+
+export default Home;
